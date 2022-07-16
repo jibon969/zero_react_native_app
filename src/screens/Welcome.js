5dff302483bf6bca473d869e15f320c2daa9b9b6
= <help.studentgrade@gmail.com>
2022-07-16 18:42:50 +0600

    added folder structure react native

---
+++ src/screens/Welcome.js
@@ -0,0 +1,15 @@
+import {
+	View,
+	Text,
+
+} from 'react-native'
+
+const Welcome = () =>{
+    return(
+        <View>
+            <Text style={{color: 'blue', fontSize: 30}} >Welcome Screen</Text>
+        </View>
+    )
+};
+
+export default Welcome;
